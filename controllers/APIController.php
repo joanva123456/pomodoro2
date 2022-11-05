2a232037a8b37fb8c8546c62782b05f3274f50c7
joan vladimir <joanvladimir2017@gmail.com>
2022-11-05 04:56:36 -0500

    Deployment

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -2,9 +2,10 @@
 
 namespace Controllers;
 
+use Model\Servicio;
 use Model\Cita;
 use Model\CitaServicio;
-use Model\Servicio;
+
 
 class APIController{
     public static function index(){
@@ -12,36 +13,39 @@ class APIController{
         echo json_encode($servicios, JSON_UNESCAPED_UNICODE);
         
     }
+    
+    public static function guardar(){
 
-    public static function guardar() {
-        
-        // Almacena la Cita y devuelve el ID
+        //guarda la cita y devuelve el id
         $cita = new Cita($_POST);
         $resultado = $cita->guardar();
 
         $id = $resultado['id'];
 
-        // Almacena la Cita y el Servicio
+        //guarda la cita y el servicio
 
-        // Almacena los Servicios con el ID de la Cita
         $idServicios = explode(",", $_POST['servicios']);
-        foreach($idServicios as $idServicio) {
+
+        foreach($idServicios as $idServicio){
             $args = [
                 'citaId' => $id,
                 'servicioId' => $idServicio
             ];
             $citaServicio = new CitaServicio($args);
-            $citaServicio->guardar();
+            $citaServicio -> guardar();
         }
 
-        echo json_encode(['resultado' => $resultado]);
+        $respuesta = [
+            'servicios' => $resultado
+        ];
+        echo json_encode($respuesta,JSON_UNESCAPED_UNICODE);
     }
 
-    public static function eliminar() {
+    public static function eliminar(){
         
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = $_POST['id'];
-            $cita = Cita::find($id);
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            
+            $cita = Cita::find($_POST['id']);
             $cita->eliminar();
             header('Location:' . $_SERVER['HTTP_REFERER']);
         }
