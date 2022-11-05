52d0c1d317cefa02feed1ae21bc863e31c3b8ad7
joan vladimir <joanvladimir2017@gmail.com>
2022-11-04 20:05:08 -0500

    First commit

---
+++ classes/Email.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+    
+    public function __construct($email, $nombre, $token)
+    {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+
+         // create a new object
+         $mail = new PHPMailer();
+         $mail->isSMTP();
+         $mail->Host = 'smtp.mailtrap.io';
+         $mail->SMTPAuth = true;
+         $mail->Port = 2525;
+         $mail->Username = '73302bd3e18fa7';
+         $mail->Password = 'c68bb7f58d1ce0';
+     
+         $mail->setFrom('joan@appsalon.com');
+         $mail->addAddress('joan@appsalon.com', 'AppSalon.com');
+         $mail->Subject = 'Confirma tu Cuenta';
+
+         // Set HTML
+         $mail->isHTML(TRUE);
+         $mail->CharSet = 'UTF-8';
+
+         $contenido = '<html>';
+         $contenido .= "<p><strong>Hola " . $this->email .  "</strong> Has Creado tu cuenta , solo debes confirmarla presionando el siguiente enlace</p>";
+         $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
+         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
+         $contenido .= '</html>';
+         $mail->Body = $contenido;
+
+         //Enviar el mail
+         $mail->send();
+
+    }
+
+    public function enviarInstrucciones() {
+
+        // create a new object
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '73302bd3e18fa7';
+        $mail->Password = 'c68bb7f58d1ce0';
+    
+        $mail->setFrom('joan@appsalon.com');
+        $mail->addAddress('joan@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Reestablece tu password';
+
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
+        $contenido .= '</html>';
+        $mail->Body = $contenido;
+
+            //Enviar el mail
+        $mail->send();
+    }
+}
