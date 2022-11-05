928e89fdab7d94652766ad0344cc54458bc398e6
joan vladimir <joanvladimir2017@gmail.com>
2022-11-05 03:35:57 -0500

    Deployment

--- includes/database.php
+++ includes/database.php
@@ -1,19 +1,16 @@
 <?php
 
-//$db = mysqli_connect('localhost', 'joan', '', '');
 $db = mysqli_connect(
     $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_DB']
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASS'], 
+    $_ENV['DB_BD']
 );
 
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_error();
+    echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
-} else{
-    echo"conectado";
-}
+} 
